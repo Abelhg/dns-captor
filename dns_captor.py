e4046091ff68bd51b628503279c85d7f5eb30b43
Abelhg <abelherrero98@gmail.com>
2018-09-22 01:51:10 +0200

    Add files via upload

--- dns_captor.py
+++ dns_captor.py
@@ -37,7 +37,7 @@ try:
     # UDP packet
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # TTL set to 0 to avoid DNS poisoning effect
-    serverSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 0)
+    #serverSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 0)
 
     try:
         serverSocket.bind(("", PORT))
@@ -49,18 +49,23 @@ try:
 
     while True:
         data, clientAddress = serverSocket.recvfrom(1024)
-        print "[DEBUG] data:", data
-        dns_response = ""
+        print "[DEBUG] query:", ":".join("{:02x}".format(ord(c)) for c in data)
+
         t_id = data[:2]
         flags = "\x81\x80" # Standard query response, no error
-        questions = "\x00\x01"
-        answerRRs = "\x00\x01"
+        questions = "\x00\x01" # 1 question
+        answerRRs = "\x00\x01" # 1 answer
         nullRRs = "\x00\x00\x00\x00"
-        query = data[12:]
-        answer = "\xC0\x0C\x00\x01\x00\x01\x00\x00\x38\x3F\x00\x04" + hex_ip
+        query = data[12:] # Query received
+        aName = "\xC0\x0C"
+        aType = "\x00\x01"
+        aClass = "\x00\x01"
+        aTTL = "\x00\x00\x00\x00" # No caching
+        aDataL = "\x00\x04"
+        answer = aName + aType + aClass + aTTL + aDataL + hex_ip
 
         dns_response = t_id + flags + questions + answerRRs + nullRRs + query + answer
-        print "[DEBUG] Response:", dns_response
+        print "[DEBUG] Response:", ":".join("{:02x}".format(ord(c)) for c in dns_response)
         serverSocket.sendto(dns_response, clientAddress)
 except KeyboardInterrupt:
     pass
